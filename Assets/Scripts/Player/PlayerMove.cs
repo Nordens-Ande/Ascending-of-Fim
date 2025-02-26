31eb4ff64d3146c777c788bff992ea2a9d953891
Elliot Nord <elliotnord@icloud.com>
2025-02-26 23:11:20 +0100

    Början av floating hitbox
    Börjat fixa en floating hitbox. Inte färdig ännu - lista också ut hur du ska göra så att animationen ser bra ut.

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -6,10 +6,24 @@ public class PlayerMove : MonoBehaviour
 
     [SerializeField] float movementSpeed;
 
+
+    [Header("Movement")]
+    [SerializeField] private float maxSpeed = 8f;
+    [SerializeField] private float acceleration = 200f;
+    [SerializeField] private AnimationCurve accelerationFactorFromDot;
+    [SerializeField] private float maxAccelForce = 150f;
+    [SerializeField] private AnimationCurve maxAccelerationForceFactorFromDot;
+    [SerializeField] private Vector3 forceScale = new Vector3(1, 0, 1);
+    [SerializeField] private float gravityScaleDrop = 10f;
+
+
     Rigidbody characterRB;
 
     private Vector3 movementInput;
     private Vector3 movementVector;
+
+    private Vector3 unitGoal;
+    private Vector3 goalVel;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -34,9 +48,22 @@ public class PlayerMove : MonoBehaviour
         characterRB.AddForce(movementVector, ForceMode.Impulse);
     }
 
+    private void CalculateMovement()
+    {
+        Vector3 unitVel = goalVel.normalized;
+
+        //float velDot = Vector3.Dot()
+    }
 
     public void OnMove(InputValue inputValue)
     {
-        movementInput = inputValue.Get<Vector2>();
+        Vector2 inputVector = inputValue.Get<Vector2>();
+
+        if (inputVector.magnitude > 1.0f)
+            inputVector.Normalize();
+
+        unitGoal.x = inputVector.x;
+        unitGoal.z = inputVector.y;
+        unitGoal.y = 0;
     }
 }
