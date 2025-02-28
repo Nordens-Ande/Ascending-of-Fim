361266709a89541072b2da1ec611e45779416ec9
Samuel <samuel.saxgarn@gmail.com>
2025-02-28 14:58:29 +0100

    Finished layout
    //TODO make scrips for buttons

---
+++ Assets/Scripts/UI/UserInterface.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class UserInterface : MonoBehaviour
+{
+
+
+    [SerializeField] GameObject menuList;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    void toggleGameObjects(string name, bool state)
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void resetPauseUI()
+    {
+
+    }
+}
