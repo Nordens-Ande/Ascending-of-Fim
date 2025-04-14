60d3b8ebcd2af812860adbbba29b85d14f46e289
MiltonChlond <Milton.chlond@gmail.com>
2025-04-14 16:17:53 +0200

    Enemy inventory, handles random weapon selection on enemy spawn.

---
+++ Assets/Scripts/Enemy/EnemyWeaponInventory.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class EnemyWeaponInventory : MonoBehaviour
+{
+    [SerializeField] EnemyShoot enemyShootScript;
+
+    [Header("Prefab References")]
+    [SerializeField] GameObject pistol;
+    //[SerializeField] GameObject rifle; //uncomment when rifle/shotgun prefabs exist and assign in inspector
+    //[SerializeField] GameObject shotgun;
+
+    GameObject weapon; //object for weapon that enemy will get, check method CreateWeapon() below
+    enum Weapon { pistol, rifle, shotgun }
+    WeaponData weaponData;
+    WeaponScript weaponScript;
+
+    void Start()
+    {
+        DecideWeapon();
+    }
+
+    void DecideWeapon() //assign random weapon from possible ones above
+    {
+        Weapon[] weapons = (Weapon[])System.Enum.GetValues(typeof(Weapon));
+        Weapon selectedWeapon = weapons[Random.Range(0, weapons.Length)];
+
+        GameObject weaponPrefab = null;
+        switch (selectedWeapon)
+        {
+            case Weapon.pistol:
+                weaponPrefab = pistol;
+                break;
+            case Weapon.rifle:
+                //weaponPrefab = rifle; //uncomment when prefabs for rifle and shotgun exist
+                break;
+            case Weapon.shotgun:
+                //weaponPrefab = shotgun;
+                break;
+        }
+
+        if(weaponPrefab != null)
+        {
+            CreateWeapon(weaponPrefab);
+            RetrieveWeaponData();
+        }
+    }
+
+    void CreateWeapon(GameObject prefab)
+    {
+        weapon = Instantiate(prefab, transform.position, transform.rotation, transform);
+    }
+
+    void RetrieveWeaponData() // get data from weapon object and send to enemyShoot Script
+    {
+        weaponScript = weapon.GetComponentInChildren<WeaponScript>();
+        weaponScript.Equip();
+        weaponData = weaponScript.GetWeaponData();
+        enemyShootScript.SetWeaponData(weaponData, weaponScript);
+    }
+
+    void Update()
+    {
+        
+    }
+}
