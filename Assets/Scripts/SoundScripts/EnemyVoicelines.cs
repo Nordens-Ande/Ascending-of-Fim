85425fff68c799a97e6fefa649ac4e89d19c2c85
Tog56 <tobias.larasigcode@gmail.com>
2025-04-26 00:41:54 +0200

    EnemyVoicelines vid idle, search, attack
    En audiosource för när enemy ska randomly säga en voicelines när i olika staider, idel, search, attack finns här. Däremot är detta enbart ett skript som kräver att andra skripts sätter igång med bools. Det finns metoder för att sätta igång de olika ljuden till true men behöver lite hjälp var i enemyskriptena dem är bäst lämpade att sätta igång. När ett stadie är sant så ska fienderna säga en voiceline randomly inom 60 sekunder.

---
+++ Assets/Scripts/SoundScripts/EnemyVoicelines.cs
@@ -0,0 +1,103 @@
+using UnityEngine;
+
+public class EnemyVoicelines : MonoBehaviour
+{
+
+    public AudioSource currentSoundEffect;
+    public AudioClip[] EnemySearchVoicelines;
+    public AudioClip[] EnemyIdleVoicelines;
+    public AudioClip[] EnemyAttackVoicelines;
+    private int RandomTime;
+    bool isEnemyIdle;
+    bool isEnemySearching;
+    bool isEnemyAttacking;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        currentSoundEffect = GetComponent<AudioSource>();
+        isEnemyIdle = false;
+        isEnemySearching = false;
+        isEnemyAttacking = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RandomTime = Random.Range(0, 10000);
+        EnemyIdleVoice();
+        EnemySearchVoice();
+        EnemyAttackVoice();
+        
+        
+    }
+
+    //Dessa metoderna hämtas av scripts som sätter igång de olika stadierna i enemy
+    //Där enemyscriptsen sätter de olika boolsen till true när det behövs
+    public void SetIdleEnemyVoiceline(bool a) 
+    { 
+        isEnemyIdle = a;
+    
+    }
+    public void SetSearchEnemyVoiceline(bool b)
+    {
+        isEnemySearching = b;
+    }
+    public void SetAttackEnemyVoiceline(bool c)
+    {
+        isEnemyAttacking = c;
+    }
+
+    //Metoderna för att de olika voicelinesen ska kunna spelas upp
+    //När deras respektive bools sätts som true i enemy scriptet kommer randomtime göra att det 
+    //behövs vara en specifik siffra för att säga en voiceline
+    //Detta kommer göra att när voiclinesen sägs är på ett spann inom 60 sekunder randomly
+    void EnemyIdleVoice() 
+    {
+        if (isEnemyIdle == true && RandomTime == 1) 
+        {
+            PlayIdleVoiceline();
+        
+        }
+    
+    }
+    void EnemySearchVoice()
+    {
+        if (isEnemySearching == true && RandomTime == 1)
+        {
+            PlaySearchVoiceline();
+
+        }
+
+    }
+    void EnemyAttackVoice()
+    {
+        if (isEnemyAttacking == true && RandomTime == 1)
+        {
+            PlayAttackVoiceline();
+        }
+
+    }
+
+
+    //De olika voicelines som kan spelas randomiserat
+    void PlayIdleVoiceline() 
+    { 
+        currentSoundEffect.clip = EnemyIdleVoicelines[Random.Range(0, EnemyIdleVoicelines.Length)];
+        currentSoundEffect.PlayOneShot(currentSoundEffect.clip, 5);
+    
+    }
+    void PlaySearchVoiceline()
+    {
+        currentSoundEffect.clip = EnemySearchVoicelines[Random.Range(0, EnemySearchVoicelines.Length)];
+        currentSoundEffect.PlayOneShot(currentSoundEffect.clip, 5);
+
+    }
+    void PlayAttackVoiceline()
+    {
+        currentSoundEffect.clip = EnemyAttackVoicelines[Random.Range(0, EnemyAttackVoicelines.Length)];
+        currentSoundEffect.PlayOneShot(currentSoundEffect.clip, 5);
+
+    }
+
+}
