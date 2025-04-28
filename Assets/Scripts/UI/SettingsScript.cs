8e48b0f30a1da6e18af1ec0b86457d3f62fe5e92
Samuel <samuel.saxgarn@gmail.com>
2025-04-28 10:28:25 +0200

    started on script
    //TODO script for settings to de-activate/active hud-element gameobjects

--- Assets/Scripts/UI/SettingsScript.cs
+++ Assets/Scripts/UI/SettingsScript.cs
@@ -1,7 +1,27 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsScript : MonoBehaviour
 {
+    [SerializeField] Toggle toggleHUDTime;
+    [SerializeField] Toggle toogleHUDScore;
+    [SerializeField] Toggle toggleHUDAmmo;
+    [SerializeField] Toggle toggleHUDMoney;
+    [SerializeField] Toggle toggleHUDHealthpoints;
+    [SerializeField] Toggle toggleHUDAnnouncement;
+
+    [Space]
+
+    [SerializeField] GameObject HUDTime;
+    [SerializeField] GameObject HUDScore;
+    [SerializeField] GameObject HUDAmmo;
+    [SerializeField] GameObject HUDMoney;
+    [SerializeField] GameObject HUDHealthpoints;
+    [SerializeField] GameObject HUDAnnouncement;
+
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
