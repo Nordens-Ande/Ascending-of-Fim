b49305309221c9150b45142d50adee7030e4f007
Elliot Nord <elliotnord@icloud.com>
2025-05-08 13:57:52 +0200

    Fixat jumpscares scriptet + nya möbler
    Nu finns det möbler som har en fiende som badar. Jag har gjort en sub-folder som heter jumpsacres för att göra det tydligare vilka möbler som är vilka.

--- Assets/Scripts/PGC Level/FurnitureJumpscare.cs
+++ Assets/Scripts/PGC Level/FurnitureJumpscare.cs
@@ -3,11 +3,27 @@ using UnityEngine;
 
 public class FurnitureJumpscare : MonoBehaviour
 {
+    enum ActivationType
+    {
+        Enter,
+        //Delayed,
+        Distance,
+        Exit
+    }
+
+    [Header("Activation")]
+    [SerializeField] private bool activated = false;
+    [SerializeField] private ActivationType activationType;
+    [SerializeField] private float distanceToActivate;
+
+    [Header("Objects")]
     [SerializeField] private EnemyAIController enemyToSpawn;
     [SerializeField] private List<Transform> spawnPositions = new List<Transform>();
     [SerializeField] private List<GameObject> objectsToDelete = new List<GameObject>();
 
     private BoxCollider hitboxCollider;
+    private Furniture furniture;
+
 
     void CreateCollider()
     {
@@ -22,6 +38,7 @@ public class FurnitureJumpscare : MonoBehaviour
     void Start()
     {
         CreateCollider();
+        furniture = gameObject.GetComponent<Furniture>();
     }
 
     public void UpdateCollider(Vector3 position, Vector3 size, bool isCentered = false)
@@ -35,33 +52,67 @@ public class FurnitureJumpscare : MonoBehaviour
         hitboxCollider.size = size;
     }
 
-    private void OnTriggerEnter(Collider other)
+    public void ActivateJumpscare()
     {
-        if (other.CompareTag("PlayerHitbox"))
+        if (activated)
+            return;
+
+        activated = true;
+        foreach (Transform trans in spawnPositions)
         {
-            Debug.Log("Player entered hitbox");
-            //Activate your script or logic when the player enters
-            //ActivateScript();
+            GameObject newEnemy = Instantiate(enemyToSpawn.gameObject);
+            newEnemy.transform.position = trans.position;
+            newEnemy.transform.rotation = trans.rotation;
+        }
+        foreach (GameObject gameObject in objectsToDelete)
+        {
+            Destroy(gameObject);
         }
     }
 
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
+        if (activationType != ActivationType.Enter)
+            return;
+
         if (other.CompareTag("PlayerHitbox"))
         {
-            Debug.Log("Player is inside hitbox");
-            //Continuously check or activate logic as long as the player stays inside
-            //ActivateScript(); //Optional for continuous check
+            Debug.Log("Player entered jumpscare hitbox");
+
+            ActivateJumpscare();
+        }
+    }
+
+    //DistanceCheck måste ske här/alternativt att ha en bool som låser upp en loop i update.
+    private void OnTriggerStay(Collider other)
+    {
+        if (activationType != ActivationType.Distance)
+            return;
+
+        if (other.CompareTag("PlayerHitbox"))
+        {
+            Vector2 furnPos = new Vector2(transform.position.x + furniture.size.x / 2f, transform.position.z + furniture.size.y / 2f);
+            Vector2 playerPos = new Vector2(other.transform.position.x, other.transform.position.z);
+
+            if (Vector2.Distance(furnPos, playerPos) < distanceToActivate)
+            {
+                Debug.Log("Player is inside minimum zone of jumpscare");
+
+                ActivateJumpscare();
+            }
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
+        if (activationType != ActivationType.Exit)
+            return;
+
         if (other.CompareTag("PlayerHitbox"))
         {
-            Debug.Log("Player exited hitbox");
-            //Deactivate or stop the logic when the player leaves
-            //DeactivateScript();
+            Debug.Log("Player exited jumpscare hitbox");
+            
+            ActivateJumpscare();
         }
     }
 
