63a478792882030ebcdbc42a3b23226cda0597e3
Hampus <hampe@mbhn.se>
2025-05-30 09:54:49 +0200

    First version of grenades
    Needs finetuning, UI interaction, reload when going through elevator, sound

---
+++ Assets/Scripts/Explosions/Grenade.cs
@@ -0,0 +1,87 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+    [SerializeField] float explosionDelay = 2f;
+    [SerializeField] float explosionRadius = 3f;
+    [SerializeField] float explosionForce = 750f;
+    [SerializeField] float explosionSpeed = 20f;
+    [SerializeField] int damage = 25;
+    [SerializeField] ParticleSystem explosionEffect;
+    [SerializeField] ExplosionSound exSound;
+
+    private HUDHandler hudHandler;
+    private bool isTriggered = false;
+
+    void Start()
+    {
+        hudHandler = FindFirstObjectByType<HUDHandler>();
+        Invoke(nameof(Explode), explosionDelay);
+    }
+    public void TakeDamage()
+    {
+        if (!isTriggered)
+        {
+            isTriggered = true;
+            Invoke(nameof(Explode), explosionDelay);
+            AudioSource.PlayClipAtPoint(exSound.explosionSound, transform.position, 5);
+        }
+    }
+    void Explode()
+    {
+        // explosion visual
+        if (explosionEffect)
+        {
+            GameObject effect = Instantiate(explosionEffect.gameObject, transform.position, Quaternion.identity);
+            ParticleSystem effectPS = effect.GetComponent<ParticleSystem>();
+            var mainPS = effectPS.main;
+            mainPS.startSpeed = explosionSpeed;
+
+            Destroy(effect, 2f);
+        }
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+        HashSet<GameObject> damagedEnemies = new HashSet<GameObject>();
+        HashSet<GameObject> damagedPlayers = new HashSet<GameObject>();
+
+        foreach (Collider nearby in colliders)
+        {
+            EnemyHealth enemy = nearby.GetComponentInParent<EnemyHealth>();
+            if (enemy != null && !damagedEnemies.Contains(enemy.gameObject))
+            {
+                enemy.ApplyDamage(damage);
+                damagedEnemies.Add(enemy.gameObject);
+                Debug.Log("Grenade damaged enemy: " + nearby.name);
+            }
+
+            PlayerHealth player = nearby.GetComponentInParent<PlayerHealth>();
+            if (player != null && !damagedPlayers.Contains(player.gameObject))
+            {
+                player.ApplyDamage(damage);
+                damagedPlayers.Add(player.gameObject);
+                Debug.Log("Grenade damaged player: " + nearby.name);
+            }
+
+            Rigidbody rb = nearby.GetComponentInParent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(explosionForce, transform.position, explosionRadius);
+                Debug.Log("Knockback applied to: " + rb.gameObject.name);
+            }
+
+            // trigger barrels
+            if (nearby.TryGetComponent(out ExplodingBarrel barrel))
+            {
+                barrel.TakeDamage();
+            }
+        }
+
+        if (hudHandler != null)
+        {
+            hudHandler.addScore(500);
+        }
+
+        Destroy(gameObject);
+    }
+}
