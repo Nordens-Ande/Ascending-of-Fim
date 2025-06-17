60db71a2b78d7056450c7d3fe3c09b33c9db254f
Samuel <162375190+Drezzz49@users.noreply.github.com>
2025-06-17 14:56:24 +0200

    Update TimerUIScript.cs

--- Assets/Scripts/UI/TimerUIScript.cs
+++ Assets/Scripts/UI/TimerUIScript.cs
@@ -1,6 +1,7 @@
 using TMPro;
 using UnityEngine;
 
+//this script is for the numbers in the time ui element to br easily editable
 public class TimerUIScript : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI timer;
